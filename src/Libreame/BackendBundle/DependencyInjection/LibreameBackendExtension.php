c8e002169794e27e198e838ad9ad8d0f0681b27e
mramirez <mramirez@MRAMIREZ.ESRICO.local>
2018-04-20 11:14:37 -0500

    [20-Abr-2018] ex4readS : Aun configurando el proyecto original

--- src/Libreame/BackendBundle/DependencyInjection/LibreameBackendExtension.php
+++ src/Libreame/BackendBundle/DependencyInjection/LibreameBackendExtension.php
@@ -23,6 +23,7 @@ class LibreameBackendExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        //$loader->load('LibreameBackendBundle\Resources\config\services.yml');
         $loader->load('services.yml');
     }
 }
